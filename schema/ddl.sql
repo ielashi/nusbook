5cb8be76759d75b6d745210bf2ae9475534baf97
icheng <ivan.cheng@gmail.com>
2011-04-05 23:34:01 +0800

    ddl from Antoine's copy

--- schema/ddl.sql
+++ schema/ddl.sql
@@ -1,48 +1,173 @@
-CREATE TABLE groups (
-  id int(11) NOT NULL AUTO_INCREMENT,
+-- phpMyAdmin SQL Dump
+-- version 3.2.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 31, 2011 at 01:38 AM
+-- Server version: 5.1.44
+-- PHP Version: 5.3.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `nusbook`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(256) NOT NULL,
-  info text,
-  PRIMARY KEY (id)
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` VALUES(1, 'Business & Finance');
+INSERT INTO `categories` VALUES(2, 'Computers');
+INSERT INTO `categories` VALUES(3, 'Cultures');
+INSERT INTO `categories` VALUES(4, 'Community');
+INSERT INTO `categories` VALUES(5, 'Internet');
+INSERT INTO `categories` VALUES(6, 'Entertainment');
+INSERT INTO `categories` VALUES(7, 'Arts');
+INSERT INTO `categories` VALUES(8, 'Family & Home');
+INSERT INTO `categories` VALUES(9, 'Games');
+INSERT INTO `categories` VALUES(10, 'Government & Politics');
+INSERT INTO `categories` VALUES(11, 'Health & Wellness');
+INSERT INTO `categories` VALUES(12, 'Hobbies & Crafts');
+INSERT INTO `categories` VALUES(13, 'Music');
+INSERT INTO `categories` VALUES(14, 'Recreation');
+INSERT INTO `categories` VALUES(15, 'Sports');
+INSERT INTO `categories` VALUES(16, 'Religion & Beliefs');
+INSERT INTO `categories` VALUES(17, 'Romance & Relationships');
+INSERT INTO `categories` VALUES(18, 'Schools & Education');
+INSERT INTO `categories` VALUES(19, 'Science');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `info` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `groups`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_members`
+--
+
+CREATE TABLE `group_members` (
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `is_admin` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`,`group_id`),
+  KEY `is_admin` (`is_admin`),
+  KEY `group_id` (`group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE group_members (
-  user_id int(11) NOT NULL,
-  group_id int(11) NOT NULL,
-  is_admin int(1) DEFAULT '0',
-  PRIMARY KEY (user_id,group_id),
-  KEY group_id (group_id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE posts (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  group_id int(11) NOT NULL,
-  poster int(11) NOT NULL,
-  post text NOT NULL,
-  post_date datetime NOT NULL,
-  reply_to int(11) DEFAULT NULL,
-  PRIMARY KEY (id),
-  KEY group_id (group_id),
-  KEY poster (poster),
-  KEY reply_to (reply_to)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE users (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  email varchar(150) NOT NULL,
-  first_name varchar(50) NOT NULL,
-  last_name varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  picture_path varchar(400) NOT NULL DEFAULT 'default/picture/path.png',
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Dumping data for table `group_members`
+--
 
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `poster` int(11) NOT NULL,
+  `post` text NOT NULL,
+  `post_date` datetime NOT NULL,
+  `reply_to` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `group_id` (`group_id`),
+  KEY `poster` (`poster`),
+  KEY `reply_to` (`reply_to`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `posts`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `picture_path` varchar(400) NOT NULL DEFAULT 'default.png',
+  `username` varchar(150) NOT NULL,
+  `firstname` varchar(256) NOT NULL,
+  `lastname` varchar(256) NOT NULL,
+  `country` varchar(128) NOT NULL,
+  `sex` char(6) NOT NULL,
+  `password` varchar(225) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `signupDate` int(11) NOT NULL,
+  `lastsignin` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` VALUES(6, 'default.png', 'default', 'Default', 'Nus', 'Canada', 'Male', '445a6c80398e10f096b3659c5b27fbe1ebe4c114717a54eb5c6d411b70cf569ce', 'default@nus.com', 1301337684, 1301337777);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `groups`
+--
+ALTER TABLE `groups`
+  ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `group_members`
+--
 ALTER TABLE `group_members`
-  ADD CONSTRAINT group_members_ibfk_1 FOREIGN KEY (user_id) REFERENCES `users` (id) ON DELETE CASCADE,
-  ADD CONSTRAINT group_members_ibfk_2 FOREIGN KEY (group_id) REFERENCES `groups` (id) ON DELETE CASCADE;
+  ADD CONSTRAINT `group_members_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `group_members_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `posts`
+--
 ALTER TABLE `posts`
-  ADD CONSTRAINT posts_ibfk_1 FOREIGN KEY (group_id) REFERENCES `groups` (id) ON DELETE CASCADE,
-  ADD CONSTRAINT posts_ibfk_2 FOREIGN KEY (poster) REFERENCES `users` (id) ON DELETE CASCADE,
-  ADD CONSTRAINT posts_ibfk_3 FOREIGN KEY (reply_to) REFERENCES posts (id) ON DELETE CASCADE;
-
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`poster`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `posts_ibfk_3` FOREIGN KEY (`reply_to`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
