af0b3cc3b01ce2ae53d07bee4cdaa0e74dee3334
Ivan Cheng <ivan.cheng@gmail.com>
2011-04-08 00:29:32 +0800

    reply tree structure for groupsview

--- groupsview.php
+++ groupsview.php
@@ -68,17 +68,38 @@
             </div> 
             
             <div id="leftColumnContent">
-            	This gets all the posts of the group and by view the post, you can see its reply, and actually reply to it. THERE IS A CHECK THAT IS MADE RIGHT HERE.</br>
+            	<!--This gets all the posts of the group and by view the post, you can see its reply, and actually reply to it. THERE IS A CHECK THAT IS MADE RIGHT HERE.-->
             	
 				<?php if($loggedInUser->isInGroup($groupInfo['id'])) { ?>
-				<a href="postadd.php?id=<?php echo $groupInfo['id'];?>">Add New Post</a><br/>
-				<br/> YOU ARE IN THE GROUP. SHOW ME ALL THE POSTS OR IF NOT OWNER, YOU CAN UNJOIN.<br />
+				<!-- YOU ARE IN THE GROUP. SHOW ME ALL THE POSTS OR IF NOT OWNER, YOU CAN UNJOIN.-->
 					<?php if(!$loggedInUser->isOwnerGroup($groupInfo['id'])) {?>
 						<a href="models/processing-forms/processing.groups.php?id=<?php echo $groupInfo['id'];?>&action=unjoin"><img style="float:right;" src="images/unjoin.png"/>
 					<?php } ?>
 					
-					This is where all the posts of the group are displayed
+					<a href="postadd.php?id=<?php echo $groupInfo['id'];?>"><img src="images/add.png"></a><br/><br/>
+					<!--This is where all the posts of the group are displayed-->
 					<?php
+					function printThread($id, $indent) {
+						$posts = fetchReplies($id);
+						$count = mysql_num_rows($posts); 
+						if($count > 0)
+						{
+							while ($post = mysql_fetch_array($posts))
+							{?>
+							<div>
+								<?php for ($i=0; $i<$indent; $i++) { echo ">"; } ?>
+								<a href="postview.php?id=<?php echo $post[0]?>"><?php echo $post['title'] ?></a> - <?php echo $post['username'] ?>, <?php echo $post['post_date'] ?> <a href="postadd.php?id=<?php echo $post['group_id'];?>&reply=<?php echo $post[0]?>"><img src="images/reply.png"></a>
+							</div>
+							<div>
+								<?php for ($i=0; $i<$indent; $i++) { echo ">"; } ?>
+								<?php echo $post['post'] ?>
+							</div>
+							<?php
+							printThread($post[0], $indent+1);
+							}
+						}
+					}
+					
 					$posts = fetchPostsOfGroup($groupInfo['id']);
 					$count = mysql_num_rows($posts); 
 					if($count > 0)
@@ -86,17 +107,18 @@
 						while ($post = mysql_fetch_array($posts))
 						{?>
 							<div>
-								<a href="postview.php?id=<?php echo $post[0]?>"><?php echo $post['title'] ?></a> - <?php echo $post['username'] ?>, <?php echo $post['post_date'] ?> <a href="postadd.php?id=<?php echo $groupInfo['id'];?>&reply=<?php echo $post[0]?>">Reply</a>
+								<a href="postview.php?id=<?php echo $post[0]?>"><?php echo $post['title'] ?></a> - <?php echo $post['username'] ?>, <?php echo $post['post_date'] ?> <a href="postadd.php?id=<?php echo $groupInfo['id'];?>&reply=<?php echo $post[0]?>"><img src="images/reply.png"></a>
 							</div>
 							<div>
 								<?php echo $post['post'] ?>
 							</div>
-						<?php
+						<?php printThread($post[0], 1); ?>
+						<br/><?php
 						}
 					}
 					?>
 						
-				<a href="postadd.php?id=<?php echo $groupInfo['id'];?>">Add New Post</a><br/>	
+				<a href="postadd.php?id=<?php echo $groupInfo['id'];?>"><img src="images/add.png"></a><br/>	
 				<?php } else { ?>
 				<br/><br />You are not part of this group. Please join it! <br /><a href="models/processing-forms/processing.groups.php?id=<?php echo $groupInfo['id'];?>&action=join"><img style="float:right;" src="images/join.png"/></a>
 				<?php } ?>
